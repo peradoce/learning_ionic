80466b1b50b69f0d980bf93b66a22a345de5598d
peradoce <gperinho20@gmail.com>
2018-04-27 19:11:59 +0000

    commit_2

---
+++ learn/IonicGreeting/src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  personName: string;
+
+
+  constructor(private toast: ToastController) {
+
+  }
+
+  showGreeting(name: string) {
+    this.toast.create({
+      message: `Olá ${name}`,
+      duration: 3000
+    }).present();
+  }
+}
